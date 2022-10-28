ee65d4d5f626f775c2cdec5d699cac589b3e0136
ShanaBiot <114422057+ShanaBiot@users.noreply.github.com>
2022-10-28 15:53:07 +0200

    Application-p-p

---
+++ application-location/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: 'connexion',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'connexion',
+    loadChildren: () => import('./connexion/connexion.module').then( m => m.ConnexionPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
