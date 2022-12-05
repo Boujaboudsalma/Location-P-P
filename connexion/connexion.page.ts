d85874c722a9b0fbdd768d63ccb7d04d88fa1bc0
ShanaBiot <114422057+ShanaBiot@users.noreply.github.com>
2022-12-05 21:55:28 +0100

    connexion/inscription

---
+++ connexion/connexion.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-connexion',
+  templateUrl: './connexion.page.html',
+  styleUrls: ['./connexion.page.scss'],
+})
+export class ConnexionPage implements OnInit {
+
+
+  constructor(
+  ) {  }
+
+  ngOnInit() {
+  }
+
+}
